b9e3b4702ef9bda0cfd00ee45e7124c22ca043cb
Vishakan-Subramanian <svishakan123@yahoo.com>
2020-01-14 15:22:00 +0530

    LinearSS

--- Ex3 Brute Force/LinearSS.py
+++ Ex3 Brute Force/LinearSS.py
@@ -1,16 +1,16 @@
 def suffixMax(arr):
 	suffix_max = []
-	for i in range(len(arr):
+	for i in range(len(arr)):
 		if(i == 0):
-			suffix_max.append(arr[j])
+			suffix_max.append(arr[i])
 		else:
-			suffix_max.append(max(suffix_max[j-1] + arr[j-1], 0))
+			suffix_max.append(max(suffix_max[i-1] + arr[i], 0))
 	
 	return suffix_max
 
 def bfMaximalArrayLinear(arr, suffix_max):
 	max_sum = 0
-	for i in range(len(arr):
+	for i in range(len(arr)):
 		max_sum = suffix_max
 
 
@@ -22,10 +22,10 @@ def main():
 	print("Array: ",arr)	
 	
 	suffix_max = suffixMax(arr)
-
-	i, j, ijsum = bfMaximalArrayLinear(arr, suffix_max)
-	print("\ni : {0}\nj : {1}\nMaximal Subarray Sum: {2}".format(i,j,ijsum))
-	print("\nSubarray : Array[{0}:{1}]\n".format(i,j),arr[i:j])
+	print(suffix_max)
+	#i, j, ijsum = bfMaximalArrayLinear(arr, suffix_max)
+	#print("\ni : {0}\nj : {1}\nMaximal Subarray Sum: {2}".format(i,j,ijsum))
+	#print("\nSubarray : Array[{0}:{1}]\n".format(i,j),arr[i:j])
 
 
 
